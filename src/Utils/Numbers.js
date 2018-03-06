691e21516f8716f692805d71f9f9e11059ed9bda
Thomas Hurtig <thomas.hurtig@nlga.niedersachsen.de>
2018-03-06 14:58:43 +0100

    fixed limits number precision

--- src/Utils/Numbers.js
+++ src/Utils/Numbers.js
@@ -20,4 +20,15 @@ export class Numbers {
 
         return precision;
     }
+
+    /**
+     * Rounds number to given precision.
+     * https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+     * @param {Number} number 
+     * @param {Number} precision    Round to [precision] decimals. Should be positive integer value.
+     */
+    static round(number, precision) {
+        var factor = Math.pow(10, precision);
+        return Math.round(number * factor) / factor;
+    }
 }
