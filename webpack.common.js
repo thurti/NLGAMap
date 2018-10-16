9c086983009e164ef6cbf7f7ff1c78c4591a9672
Thomas Hurtig <thomas.hurtig@nlga.niedersachsen.de>
2018-10-16 16:03:13 +0200

    compile leaflet-piemarker since its written in es6

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
+                exclude: /(node_modules)\/(?!leaflet-piemarker)/,
                 use: [
                 {
                     loader: 'babel-loader',
